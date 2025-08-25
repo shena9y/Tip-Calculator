2df343a23214e47dbe6f096bffa9b212f9cfa6b9
Mohammed Hamdy <119974638+shena9y@users.noreply.github.com>
2025-08-26 00:28:23 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("calculate");
+const bill = document.getElementById("bill");
+const tip = document.getElementById("tip");
+const total = document.getElementById("total");
+
+function calculatetotal() {
+  const billValue = bill.value;
+  const tipValue = tip.value;
+  const totalValue = billValue * (1 + tipValue / 100);
+  total.innerText = totalValue.toFixed(2) + " $";
+}
+
+btn.addEventListener("click", calculatetotal);
